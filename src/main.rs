212ba1c26842b3d438683d5de04602552b6f777d
Brent Bettis <brent.bettis@gmail.com>
2020-11-02 00:27:52 -0800

    adding configuration file

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
+extern crate config;
+
 use std::io::prelude::*;
 use std::time::Duration;
 use std::fs::File;
 use std::io::BufReader;
 use std::thread::sleep;
 use gpio_cdev::{Chip, LineRequestFlags};
+use config::{ConfigError, Config, File as ConfigFile};
+use serde::Deserialize;
 
 #[derive(Debug)]
 enum FanError {
     GpioError(gpio_cdev::Error),
     IoError(std::io::Error),
     ParseIntError(std::num::ParseIntError),
+    ConfigError(ConfigError),
 }
 
 impl From<gpio_cdev::Error> for FanError {
@@ -30,6 +35,36 @@ impl From<std::num::ParseIntError> for FanError {
     }
 }
 
+impl From<ConfigError> for FanError {
+    fn from (error: ConfigError) -> Self {
+        FanError::ConfigError(error)
+    }
+}
+
+#[derive(Deserialize, Debug)]
+struct Settings {
+    gpio_path: String,
+    gpio_pin: u32,
+    temp_max: u32,
+    temp_path: String,
+    retry_interval_hot: u64,
+    retry_interval_cool: u64,
+}
+
+impl Settings {
+    fn new() -> Result<Self, ConfigError> {
+        let mut settings = Config::new();
+        settings.set_default("gpio_path", String::from("/dev/gpiochip0"))?;
+        settings.set_default("gpio_pin", 18)?;
+        settings.set_default("temp_max", 40)?;
+        settings.set_default("temp_path", String::from("/sys/class/thermal/thermal_zone0/temp"))?;
+        settings.set_default("retry_interval_hot", 30)?;
+        settings.set_default("retry_interval_cool", 120)?;
+        settings.merge(ConfigFile::with_name("/etc/default/rpi-fan").required(false))?;
+
+        settings.try_into()
+    }
+}
 fn main() {
     std::process::exit(match run_fan() {
         Ok(()) => 0,
@@ -41,24 +76,23 @@ fn main() {
 }
 
 fn run_fan() -> std::result::Result<(), FanError> {
-    let mut chip = Chip::new("/dev/gpiochip0")?;
-    let line = 18u32;
-    let max_temp = 40000u32;
+    let settings = Settings::new()?;
+    let mut chip = Chip::new(settings.gpio_path)?;
     let handle = chip
-        .get_line(line)?
+        .get_line(settings.gpio_pin)?
         .request(LineRequestFlags::OUTPUT, 1, "rpi-fan")?;
     loop {
-        let file = File::open("/sys/class/thermal/thermal_zone0/temp")?;
+        let file = File::open(settings.temp_path.as_str())?;
         let mut buf_reader = BufReader::new(file);
         let mut line = String::new();
         let _len = buf_reader.read_line(&mut line)?;
         let temp: u32 = line.trim().parse()?;
-        if temp > max_temp {
+        if temp > (settings.temp_max * 1000) {
             handle.set_value(1)?;
-            sleep(Duration::from_secs(120));
+            sleep(Duration::from_secs(settings.retry_interval_hot));
         } else {
             handle.set_value(0)?;
-            sleep(Duration::from_secs(30));
+            sleep(Duration::from_secs(settings.retry_interval_cool));
         }
     }
 }
